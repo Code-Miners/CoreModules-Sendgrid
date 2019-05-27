7bb7387f71fb8356be73ecf8a77adcd8b96450fc
Simon <simon.proctor@codeminers.co.uk>
2019-05-27 01:07:53 +0100

    LGPL release of our Sendgrid wrapper

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# CoreModules-Sendgrid
-Simple wrapper to Sendgrid
+#  Codeminers Libraries: Sendgrid Interface
+
+This library is an interface over Sendgrid that in turn makes use of an open source library for calling their system. 
+
+This library is copyright Code Miners Limited and is provided as an accelerator to existing and new customers 
+as part of ongoing project work.
+
+No liability is assumed or implied. All bug fixes are handled internally and are not billable to the client(s).
+
+## Setup and configuration
+
+The module installs as a Nuget package from the company Nuget repo. To configure, the following key needs to be 
+added to the application app settings.
+
+> CodeMiners:Modules:SendGrid:ApiKey
+
+
+If you don't want to use appsettings, then make your own class that implements 
+
+> Core.Modules.Communications.Sendgrid.ICommunicationConfiguration 
+
+and register with the DI container for the project. Customisations like this do not belong in this module unless 
+they are sufficiently generic (and are not billable to the client).
+
+## Roadmap
+
+Right now we don't have a .NET Core version of this library but that's in the pipeline. A lot of the work we do use
+platforms and systems where .NET Core isn't available. That said, here's what we're thinking about:
+
+* Embedding images
+* Better use of immutable types
+* .NET Core
+
+
+## Where this code has come from
+
+In developing our website we were throwing some code together and decided we'd start releasing it in case someone found 
+it useful. So we've repackaged our code into 'modules', adding in Sonar settings and tests then released them into
+the wild.
+
+If this code is of use to you, great! If not, no problem, there's a fancy search feature top left of this page. Happy 
+hunting.
+
+:rocket:
---
+++ src/Core.Modules.Communications.Sendgrid/Models/EmailAddress.cs
@@ -0,0 +1,199 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EmailAddress.cs" company="Code Miners Limited">
+//  Copyright (c) 2019 Code Miners Limited
+//   
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU Lesser General Public License for more details.
+//  
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.If not, see<https://www.gnu.org/licenses/>.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Core.Modules.Communications.Sendgrid.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents an email address as an immutable value 
+    /// </summary>
+    [Serializable]
+    public sealed class EmailAddress : IEquatable<EmailAddress>
+    {
+        private readonly bool empty;
+
+        /// <summary>
+        /// The underlying email address value
+        /// </summary>
+        private readonly string value;
+
+        /// <summary>
+        /// Define EmailAddress.Empty
+        /// </summary>
+        public static EmailAddress Empty => new EmailAddress();
+
+        /// <summary>
+        /// default constructor - sets value to emailaddress.empty
+        /// </summary>
+        private EmailAddress()
+        {
+            value = string.Empty;
+            empty = true;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmailAddress"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public EmailAddress(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Invalid value for email address", nameof(value));
+            }
+
+            this.value = value;
+        }
+
+        /// <summary>
+        /// Attempts to build a new emailaddress instance by testing and using the candidate emailaddress. If this
+        /// is invalid then the emailaddress instance is Empty and the method returns false
+        /// </summary>
+        /// <param name="candidate">The candidate email address</param>
+        /// <param name="emailAddress">The emailaddress instance</param>
+        /// <returns>
+        /// Flag indicating successful parse or not
+        /// </returns>
+        public static bool TryParse(string candidate, out EmailAddress emailAddress)
+        {
+            if (string.IsNullOrWhiteSpace(candidate))
+            {
+                emailAddress = Empty;
+                return false;
+            }
+
+            emailAddress = new EmailAddress(candidate.Trim());
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(EmailAddress emailAddress)
+        {
+            if (emailAddress == null)
+            {
+                return true;
+            }
+
+            return emailAddress.empty;
+        }
+
+        /// <summary>
+        /// Performs an implicit conversion from <see cref="EmailAddress"/> to <see cref="System.String"/>.
+        /// </summary>
+        /// <param name="emailAddress">The email address</param>
+        /// <returns>
+        /// The result of the conversion.
+        /// </returns>
+        public static implicit operator string(EmailAddress emailAddress)
+        {
+            return emailAddress.value;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>The string representation</returns>
+        public override string ToString()
+        {
+            return value;
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="other">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Equals(EmailAddress other)
+        {
+            if (ReferenceEquals(other, null))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return value.Equals(other.value);
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            EmailAddress b = obj as EmailAddress;
+            if (ReferenceEquals(b, null))
+            {
+                return false;
+            }
+
+            return Equals(b);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return value.GetHashCode();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static bool operator ==(EmailAddress a, EmailAddress b)
+        {
+            if (ReferenceEquals(a, null))
+            {
+                return ReferenceEquals(b, null);
+            }
+
+            return a.Equals(b);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(EmailAddress a, EmailAddress b)
+        {
+            return !(a == b);
+        }
+    }
+}
